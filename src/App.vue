8970c98c6a5ec341aa594dc3667a3962403059b1
Abdullah Raghib <103993447+abdullah-vidyamantra@users.noreply.github.com>
2022-04-28 18:51:05 +0530

    df

---
+++ src/App.vue
@@ -0,0 +1,57 @@
+<template>
+  <div>
+    <button @click="showComp">show</button>
+    <div v-if="show">
+      <LoaderComp />
+    </div>
+  </div>
+</template>
+
+<script>
+import LoaderComp from "./components/LoaderComp.vue"
+import LoadingComp from "./components/LoadingComp.vue";
+import ErrorComp from "./components/ErrorComp.vue";
+// import { defineAsyncComponent } from '@vue/runtime-core';
+// const LoaderComp = defineAsyncComponent({
+//   // the loader function
+//   loader: () => import('./components/LoaderComp.vue'),
+
+//   // A component to use while the async component is loading
+//   loadingComponent: LoadingComp,
+//   // Delay before showing the loading component. Default: 200ms.
+//   delay: 200,
+
+//   // A component to use if the load fails
+//   errorComponent: ErrorComp,
+//   // The error component will be displayed if a timeout is
+//   // provided and exceeded. Default: Infinity.
+//   timeout: 1000
+// })
+export default {
+  name: "App",
+  data() {
+    return {
+      show: false,
+    };
+  },
+  components: {
+    LoaderComp,LoadingComp,ErrorComp
+  },
+  methods: {
+    showComp() {
+      this.show = true;
+    },
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/LoaderComp.vue
@@ -0,0 +1,15 @@
+<template>
+  <h1>Loader hu main</h1>
+  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus incidunt dolores, doloremque eos laudantium sunt impedit ipsa id adipisci, numquam non tempora odio nisi sed. Voluptatem adipisci iusto commodi minima quam deleniti sunt rerum fuga labore omnis non ut, veritatis optio! Voluptatibus maiores nam placeat facilis illo quam, numquam consectetur pariatur nihil, molestiae veritatis sint ad dicta aut quidem labore beatae minima. Ut aliquid esse quasi dignissimos sed possimus officiis libero suscipit provident labore repellendus fugit maxime porro saepe, quidem nisi. Magnam modi reiciendis voluptate facilis, natus ipsam optio possimus et quod obcaecati unde nemo quo odio eos animi ipsum adipisci nulla consequatur quisquam sequi nesciunt illo delectus cum. Corporis totam libero deleniti ad? Error quas voluptates at nisi voluptatibus eveniet amet cupiditate repellat id ipsam unde vero quae, deleniti facilis eaque quis iure nostrum debitis delectus inventore ad nihil impedit vel a. Exercitationem repellat maiores quam impedit eum natus quasi voluptatum harum perferendis tempora, dolorum, expedita ratione. Impedit reiciendis labore perspiciatis voluptate tenetur fuga, earum vero voluptatem, doloribus nam odit, autem recusandae dicta laudantium reprehenderit! Enim optio nobis amet repellendus ratione libero reprehenderit fugiat repudiandae tempore illo qui laboriosam eaque, reiciendis voluptatem atque cupiditate iusto voluptatibus blanditiis vitae? Maiores.</p>
+ 
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
